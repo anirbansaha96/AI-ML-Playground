bfb315c3a66f45660f5183ae8e4a995de670cd86
Anirban Saha <52232270+anirbansaha96@users.noreply.github.com>
2020-01-24 23:09:13 +0530

    Update chunking.py

--- chunking.py
+++ chunking.py
@@ -1,11 +1,14 @@
-#We use chunking to calculate the mean of a 4999998*25 size data.
+#We use chunking to calculate the mean and standard deviation of a 4999998*25 size data.
 import numpy as np
 import pandas as pd
 total=np.zeros((25,1));
+totalsquare=np.zeros((25,1));
 totalrows=0;
 for chunk in pd.read_csv('dataLarge',chunksize=10000,delimiter=' ',index_col=0,header=None):
     dataset=chunk;
     total+=pd.DataFrame(dataset.sum());
+    totalsquare+=pd.DataFrame((dataset**2).sum());
     [nrows,ncols]=dataset.shape;
     totalrows+=nrows;
-mean=total/totalrows;        
+mean=total/totalrows;    
+variance=((totalsquare/totalrows)-(mean**2))**0.5;
